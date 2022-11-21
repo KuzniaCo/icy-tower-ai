8c68adcc7d57bf7095771509d55b90da08fd999a
MateuszMackowiak <mackowiak.mateusz2001@gmail.com>
2022-11-22 00:23:38 +0100

    displaying platforms

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 # Import the pygame library and initialise the game engine
 import pygame
-wallImg = pygame.image.load('leftWall.png')
+import random
+
+
+leftPlatformSegmentImg=pygame.image.load('platformLeft.png')
+middlePlatformSegmentImg=pygame.image.load('platformMiddle.png')
+rightPlatformSegmentImg=pygame.image.load('platformRight.png')
+
+leftWallImg = pygame.image.load('leftWall.png')
+rightWallImg = pygame.image.load('rightWall.png')
 backgroundSlice = pygame.image.load('backgroundSlice.png')
+kuzniaLogoImg = pygame.image.load('kuzniaLogo.png')
 playerStandingAnim=[pygame.image.load("./PlayerStanding/1.png"),pygame.image.load("./PlayerStanding/2.png")]
 pygame.init()
 
@@ -12,7 +21,18 @@ playerSpeed=3
 offset=0
 playerMoveVector=[0,0]
 
-platformColliders=[]
+def generatePlatform(leftBounds,rightBounds,heightOfGameWindow):
+    posX=random.randint(leftBounds-40,rightBounds+40)#40 is player width, so -40 makes minimal gap from wall 2x player width
+    posX=posX%40
+    posY=-20#heightOfGameWindow is bottom, platform height is 20
+    numberOfPlatformSegments=random.randint(2,7)#I made it so platform has maximum 7 segments
+    return [posX,posY,numberOfPlatformSegments]
+platforms=[]#[[posX,posY,numberOfPlatformSegments],[posX,posY,numberOfPlatformSegments],[posX,posY,numberOfPlatformSegments], ... ]
+def displayPlatform(screen,platform,offset,screenSizeY):
+    screen.blit(leftPlatformSegmentImg,(platform[0],platform[1]+offset))
+    for i in range(1,platform[2]-1):
+        screen.blit(middlePlatformSegmentImg, (platform[0] + i * 40, platform[1] + offset))
+    screen.blit(rightPlatformSegmentImg,(platform[0]+platform[2]*40-40,platform[1]+offset))
 
 # Open a new window
 size = (800, 600)
@@ -25,6 +45,7 @@ carryOn = True
 # The clock will be used to control how fast the screen updates
 clock = pygame.time.Clock()
 
+platforms.append(generatePlatform(40,size[0]-40,size[1]))
 # -------- Main Program Loop -----------
 while carryOn:
     # --- Main event loop
@@ -56,17 +77,27 @@ while carryOn:
     if (playerPosY < 120):
         offset = offset + playerSpeed
         offset = offset % 40
-        print(offset)
+        playerPosY=playerPosY+playerSpeed/2
+        #print(offset)
+        
+    #render left walls and background and right walls
     for i in range(0,45):
         if(playerPosY<120):
             screen.blit(backgroundSlice, (0, -600 + i * 40+offset))
-            screen.blit(wallImg, (0, -600 + i * 40+offset))
+            screen.blit(leftWallImg, (0, -600 + i * 40+offset))
+            screen.blit(rightWallImg, (size[0]-40, -600 + i * 40 + offset))
         else:
             screen.blit(backgroundSlice, (0, -600 + i * 40))
-            screen.blit(wallImg, (0, -600 + i * 40))
+            screen.blit(leftWallImg, (0, -600 + i * 40))
+            screen.blit(rightWallImg, (size[0]-40, -600 + i * 40))
     playerPosY+=playerMoveVector[1]
     playerPosX+=playerMoveVector[0]
     screen.blit(playerStandingAnim[0],(playerPosX,playerPosY))
+    for i in platforms:
+        if(playerMoveVector[1]==-1):
+            i[1]=i[1]+playerSpeed
+            print(i[1])
+        displayPlatform(screen,i,offset,size[1])
 
 
 
